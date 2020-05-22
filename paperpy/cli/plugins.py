dab00bbd6f56f849e033e5b6ecad19c20439caab
Robin De Schepper <robin.deschepper93@gmail.com>
2020-05-22 22:56:40 +0200

    Pluggable CLI (#3)
    * Added the Action interface
    
    * Added __version__
    
    * Added `exceptions.py` for semantic exception classes
    
    * Added plugin discovery mechanism
    
    * Added CLI module initialisation
    
    * Added package setup.

---
+++ paperpy/cli/plugins.py
@@ -0,0 +1,15 @@
+import pkg_resources
+from ..exceptions import *
+
+def discover(category, *args, **kwargs):
+    plugins = []
+    for plugin in pkg_resources.iter_entry_points("paperpy." + category):
+        try:
+            advert = plugin.load()
+            # Load the advertised object, and create an instance of it.
+            instance = advert(*args, **kwargs)
+            instance.__name__ = plugin.name
+            plugins.append(instance)
+        except:
+            raise PluginError("Could not instantiate the `{}` plugin".format(plugin.name))
+    return plugins
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os, sys
+import setuptools
+
+# Get text from README.txt
+with open('README.md', 'r') as fp:
+    readme_text = fp.read()
+
+# Get __version__ without importing
+with open('paperpy/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__ = '):
+            exec(line)
+            break
+
+setuptools.setup(
+    name='paperpy',
+    version=__version__,
+    description='Tools for simplifying the preparation of version-controlled and well-written academic papers.',
+    license='MIT',
+    keywords='paper academia academic writing write',
+    author='Robin De Schepper',
+    author_email='robingilbert.deschepper@unipv.it',
+    url='https://github.com/paperpy/paperpy',
+    long_description=readme_text,
+    long_description_content_type='text/markdown',
+    packages=['paperpy'],
+    include_package_data = True,
+    package_data={"paperpy": ["data/**/*"]},
+    entry_points={
+        "console_scripts": ["paperpy = paperpy.cli:handle_command"],
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
